9587e9a64bdfc4b21bfd4a771b1089e986af481a
Alper Kurtul <alperkurtul@gmail.com>
2019-11-02 22:01:40 +0300

    upload to repository

--- src/main/java/com/github/alperkurtul/firebaserealtimedatabase/annotation/FirebaseRealtimeDbRepoService.java
+++ src/main/java/com/github/alperkurtul/firebaserealtimedatabase/annotation/FirebaseRealtimeDbRepoService.java
@@ -1,11 +1,5 @@
 package com.github.alperkurtul.firebaserealtimedatabase.annotation;
 
-//public interface FirebaseRealtimeDbRepoService<AUTH, DOC, ID> {
-//
-//    DOC read(AUTH obj1, DOC obj2);
-//
-//}
-
 public interface FirebaseRealtimeDbRepoService<FC, FD, ID> {
 
     FD read(FC fc);
