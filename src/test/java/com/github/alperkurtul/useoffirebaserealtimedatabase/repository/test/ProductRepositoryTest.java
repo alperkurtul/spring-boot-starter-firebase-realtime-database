e53f83b24bce7ba3e96dfaf879dc1628498c5fc9
Alper Kurtul <alperkurtul@gmail.com>
2019-11-03 14:39:44 +0300

    upload to repository

--- src/test/java/com/github/alperkurtul/useoffirebaserealtimedatabase/repository/test/ProductRepositoryTest.java
+++ src/test/java/com/github/alperkurtul/useoffirebaserealtimedatabase/repository/test/ProductRepositoryTest.java
@@ -32,6 +32,7 @@ public class ProductRepositoryTest {
         public RestTemplate restTemplate() {
             return new RestTemplate();
         }
+
         @Bean
         public ProductRepository productRepository() {
             return new ProductRepository();
